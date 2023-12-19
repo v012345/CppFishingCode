0b3410e5aa72095402d32c8305f138fe50fb2ad8
v012345 <v138468@gmail.com>
2023-12-19 11:50:58 +0800

    删除不使用断言

--- stdafx.h
+++ stdafx.h
@@ -45,17 +45,6 @@ template <class Interface> inline void SafeRelease(Interface** ppInterfaceToRele
     }
 }
 
-#ifndef Assert
-#if defined(DEBUG) || defined(_DEBUG)
-#define Assert(b)                                                                                                                                                                                      \
-    do {                                                                                                                                                                                               \
-        if (!(b)) { OutputDebugStringA("Assert: " #b "\n"); }                                                                                                                                          \
-    } while (0)
-#else
-#define Assert(b)
-#endif // DEBUG || _DEBUG
-#endif
-
 #ifndef HINST_THISCOMPONENT
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 #define HINST_THISCOMPONENT ((HINSTANCE) & __ImageBase)
